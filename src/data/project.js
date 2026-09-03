88cd62eb11962aa545c821f8f82ac93d23c74a9d
Ohmywanzz <marwansetyawan032@gmail.com>
2026-09-03 12:45:49 +0700

    update

---
+++ src/data/project.js
@@ -0,0 +1,18 @@
+import { Description } from "@headlessui/react";
+import { Title } from "@mui/icons-material";
+import { imageListClasses } from "@mui/material";
+import { Github } from "lucide-react";
+
+const project = [
+
+{
+    id: 4,
+    title: "FoxSquad",
+    Description:
+    "game the vn fanmade",
+    image: "/public/bg.jpg",
+    demo: "https://www.mediafire.com/file/60x1rht1yxfdwoy/FoxSquad-0.1-win.zip/file",
+    Github: "htpps://github.com/Ohmywanzz",
+}
+
+]
